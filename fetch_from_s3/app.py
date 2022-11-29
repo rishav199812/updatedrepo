115730b9c08d4277d4269de088b0089254d45b94
rishav199812 <64005862+rishav199812@users.noreply.github.com>
2022-11-29 13:39:44 +0530

    Create app.py

---
+++ fetch_from_s3/app.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class DataMappingTest(unittest.TestCase):
+	@classmethod
+	def setUpClass(cls):
+		pass
+	
+	def test_data_mapping_with_only_passing_item(self):
+		assert True
+
+
+if __name__ == "__main__":
+    unittest.main()
